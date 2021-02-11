184b72e7a70cd3a53d912b577acb7a164c17153d
Muhammad Ali <59179161+080hackers@users.noreply.github.com>
2021-02-11 08:54:24 +0100

    Fixed

--- TermuxTheme.sh
+++ TermuxTheme.sh
@@ -4,38 +4,33 @@
 #If You Take Money For This Little Tool Bash Script Please Look At Your Self Again And Shame To Your
 clear
 echo ""
-echo "      [SUBSCRIBE BAJE TECH YOUTUBE CHANNEL FOR MORE]"|lolcat
-echo "         _____   _____ _   _ _____ __  __ _____"|lolcat
-echo "        |_   _| |_   _| | | | ____|  \/  | ____|"|lolcat
-echo "          | |_____| | | |_| |  _| | |\/| |  _|"  |lolcat
-echo "          | |_____| | |  _  | |___| |  | | |___" |lolcat
-echo "          |_|     |_| |_| |_|_____|_|  |_|_____| V1.2"|lolcat
-echo "          [Coded By (Baje Tech)  080HACKERS CEO]"|lolcat
+echo " [SUBSCRIBE BAJE TECH YOUTUBE CHANNEL FOR MORE]"|lolcat
+echo "    _____   _____ _   _ _____ __  __ _____"|lolcat
+echo "   |_   _| |_   _| | | | ____|  \/  | ____|"|lolcat
+echo "     | |_____| | | |_| |  _| | |\/| |  _|"  |lolcat
+echo "     | |_____| | |  _  | |___| |  | | |___" |lolcat
+echo "     |_|     |_| |_| |_|_____|_|  |_|_____| V1.2"|lolcat
+echo "     [Coded By (Baje Tech)  080HACKERS CEO]"|lolcat
 sleep 1.0
 echo ""
 echo "This Will Change Your Termux Home Theme Color To Look Aweasome"|lolcat
 echo ""
 read -p 'Please Press Enter To Continue Or Press CTRL + Z To Cancel'
 echo ""
+sleep 0.0
 echo "Ok Your Termux Theme Will Change Now"|lolcat
 sleep 2.0
-echo " Please Wait For 10 Seconds To Make The Change..."|lolcat
+echo " Please Wait For 8 Seconds To Make The Change..."|lolcat
 cd
 cd /$HOME
 rm //data/data/com.termux/files/usr/etc/bash.bashrc
 cd
 mv /data/data/com.termux/files/home/TermuxTheme/code/bash.bashrc //data/data/com.termux/files/usr/etc/bash.bashrc
-sleep 10.0
+sleep 8.0
 echo "Finished, Your Termux Now Looking Aweasome"|lolcate
 sleep 1.0
 echo "Due To Confirmination Of This Chnages Your Termux Will Now Close By It Self"|lolcate
 echo "In Other To Let You See The Changes"|lolcate
 sleep 1.0
 echo "Exiting... Please Relounch The Termux Again"|lolcat
-sleep 1.0
-exit
-exit
-exit
-exit
-exit
-end 
+echo "Exit From Termux And Open It Again To Se The Changes"|lolcat
